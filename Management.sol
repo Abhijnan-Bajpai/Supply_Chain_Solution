53b6b1ec6c4b533a888de3ae031cffd182164fc2
Abhijnan-Bajpai <57059194+Abhijnan-Bajpai@users.noreply.github.com>
2021-08-12 08:49:12 +0530

    Added comments

--- Management.sol
+++ Management.sol
@@ -3,19 +3,24 @@ pragma solidity ^0.6.0;
 
 contract ItemManager {
     
+    // Saves the present state of each item in the supply chain
     enum SupplyChainSteps{Created, Paid, Delivered}
-    
+     
+    // An object which defines an item in the supply chain
     struct S_item {
         ItemManager.SupplyChainSteps _step;
         string _identifier;
         uint _priceInWei;
     }
     
+    // A mapping of all objects in the supply chain for easier access and keeping note of history
     mapping(uint => S_item) public items;
     uint index;
     
+    // Emitting an event on the state change of an object
     event SupplyChainStep(uint _index, uint step);
     
+    // Creating a new object by saving all the parameters of the item object
     function createItem(string memory _identifier, uint _priceInWei) public {
         items[index]._priceInWei = _priceInWei;
         items[index]._identifier = _identifier;
@@ -24,6 +29,7 @@ contract ItemManager {
         index++;
     }
     
+    // Triggering payment when the given conditions satisfy
     function triggerPayment(uint _index) public payable {
         require(items[_index]._priceInWei <= msg.value, "Not fully paid");
         require(items[_index]._step == SupplyChainSteps.Created, "Item further in supply chain");
@@ -31,6 +37,7 @@ contract ItemManager {
         emit SupplyChainStep(_index, uint(items[_index]._step));
     }
     
+    // Triggering delivery when the given conditions satisfy
     function triggerDelivery(uint _index) public {
         require(items[_index]._step == SupplyChainSteps.Paid, "Item further in supply chain");
         items[_index]._step = SupplyChainSteps.Delivered;
